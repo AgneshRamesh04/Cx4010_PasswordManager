a744aebcf702483c52649566b5999eaca152a30d
Agnesh Ramesh <74037406+AgneshRamesh04@users.noreply.github.com>
2021-11-24 23:15:05 +0800

    Add files via upload

---
+++ Backend/random_password_generator.py
@@ -0,0 +1,17 @@
+#from encryption import EncryptDeCrypt
+import json
+import secrets
+import string
+
+
+'''
+Cryptographically secure random numbers
+'''
+def generate_password():
+    # secure password
+    password = ''.join((secrets.choice(string.ascii_letters + string.digits + string.punctuation) for i in range(18)))
+    return password
+
+
+def password_strength(password):
+    print(password)
